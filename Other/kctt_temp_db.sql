4e8d17d8e8103c69b4af3bd6f8ca2a7db5f6ee3c
Domerk <orochimaximov@mail.ru>
2016-04-27 16:08:11 +0300

    Добаботанная версия скрипта базы

--- Other/kctt_temp_db.sql
+++ Other/kctt_temp_db.sql
@@ -1,37 +1,38 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS Запись;
 DROP TABLE IF EXISTS Учащийся;
 
 CREATE TABLE Учащийся (
 -- Поля
-"Фамилия"			VARCHAR(60)		NOT NULL,
-"Имя"				VARCHAR(60)		NOT NULL,
-"Отчество"		    VARCHAR(60),
-"Тип документа"	  	VARCHAR(60)		NOT NULL,
-"Номер документа"	VARCHAR(60)		NOT NULL,
-"Пол"			    VARCHAR(3)		NOT NULL,
-"Год рождения"	  	DATE,
+`Фамилия`			VARCHAR(60)		NOT NULL,
+`Имя`				VARCHAR(60)		NOT NULL,
+`Отчество`		    VARCHAR(60),
+`Тип документа`	  	VARCHAR(60)		NOT NULL,
+`Номер документа`	VARCHAR(60)		NOT NULL,
+`Пол`			    VARCHAR(3)		NOT NULL,
+`Год рождения`	  	DATE,
 
-"Район школы"		  VARCHAR(60),
-"Школа"			      VARCHAR(60),
-"Класс"			      VARCHAR(5),
+`Район школы`		  VARCHAR(60),
+`Школа`			      VARCHAR(60),
+`Класс`			      VARCHAR(5),
 
-"Родители"		    VARCHAR(200),
-"Домашний адрес"	VARCHAR(200),
-"Телефон"   			VARCHAR(100),
-"e-mail"     			VARCHAR(100)
+`Родители`		    VARCHAR(200),
+`Домашний адрес`	VARCHAR(200),
+`Телефон`   			VARCHAR(100),
+`e-mail`    			VARCHAR(100),
 
-PRIMARY KEY ("Тип документа", "Номер документа")
+PRIMARY KEY (`Тип документа`, `Номер документа`)
 );
 
 CREATE TABLE Запись(
 -- Поля
-"Тип документа"	  	VARCHAR(60)		NOT NULL,
-"Номер документа"	VARCHAR(60)		NOT NULL,
-"Объединение"		VARCHAR(60)		NOT NULL
+`Тип документа`	  	VARCHAR(60)		NOT NULL,
+`Номер документа`	VARCHAR(60)		NOT NULL,
+`Объединение`		VARCHAR(60)		NOT NULL,
 -- Параметры
-PRIMARY KEY ("Тип документа", "Номер документа", "Объединение"),
-FOREIGN KEY ("Тип документа", "Номер документа")  REFERENCES Учащийся("Тип документа", "Номер документа")
+PRIMARY KEY (`Тип документа`, `Номер документа`, `Объединение`),
+FOREIGN KEY (`Тип документа`, `Номер документа`)  REFERENCES Учащийся(`Тип документа`, `Номер документа`)
 );
 
 
